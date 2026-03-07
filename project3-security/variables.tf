cf934d6701e928c47192f034578c486d7c65841f
Sisay Abebe <256173725+SisayAbebe-dev@users.noreply.github.com>
2026-03-07 12:24:41 -0500

    Define Terraform variables for AWS configuration
    Added variables for region, instance type, key name, public IP, and AMI ID in Terraform configuration.

--- project3-security/variables.tf
+++ project3-security/variables.tf
@@ -1,1 +1,28 @@
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for MongoDB nodes"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your public IPv4 address with /32"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI ID"
+  type        = string
+  default     = "ami-0f3caa1cf4417e51b"
+}
