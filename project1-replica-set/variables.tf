aa05182c80fc84c8eff1c582bc98388e7e820a3a
Sisay Abebe <256173725+SisayAbebe-dev@users.noreply.github.com>
2026-03-19 14:10:04 -0400

    Update variables.tf

--- project1-replica-set/variables.tf
+++ project1-replica-set/variables.tf
@@ -1,1 +1,14 @@
 
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "my_ip" {
+  description = "71.191.131.44"
+  type        = string
+}
+
+variable "key_name" {
+  description = "sisay-keypair"
+  type        = string
