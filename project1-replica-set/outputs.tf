b9c9db1ccbf82997a4f7963f3f4030387e95ace7
Sisay Abebe <256173725+SisayAbebe-dev@users.noreply.github.com>
2026-03-19 14:14:18 -0400

    Update outputs.tf

--- project1-replica-set/outputs.tf
+++ project1-replica-set/outputs.tf
@@ -1,1 +1,9 @@
 
+output "bastion_public_ip" {
+  description = "Connect to this IP first to access my private network"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "mongodb_private_ips" {
+  description = "The private IPs of my MongoDB nodes"
+  value       = aws_instance.mongo_nodes[*].private_i
